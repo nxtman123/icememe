702bb44e9478925936b09f4e6eea061f3bec6cb5
Kurtis Jantzen <kjjantze@ucalgary.ca>
2019-03-23 15:33:16 -0600

    Lint the migrations files

--- migrations/20190321232837_create_users_table.js
+++ migrations/20190321232837_create_users_table.js
@@ -1,29 +1,26 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function(table) {
-    table.increments('user_id');
-    table.string('username', 20).notNullable();
-    table.string('password', 100).notNullable();
-    table.string('email', 50).notNullable();
-    table.timestamp('date_created').defaultTo(knex.fn.now());
+exports.up = knex => knex.schema.createTable('users', (table) => {
+  table.increments('user_id');
+  table.string('username', 20).notNullable();
+  table.string('password', 100).notNullable();
+  table.string('email', 50).notNullable();
+  table.timestamp('date_created').defaultTo(knex.fn.now());
 
-    table.unique('username');
-    table.unique('email');
-  })
-}
+  table.unique('username');
+  table.unique('email');
+});
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
-}
+exports.down = knex => knex.schema.dropTable('users');
 
 /*
 CREATE TABLE IF NOT EXISTS users (
-	user_id			INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username		VARCHAR(20) NOT NULL,
-    `password`  	VARCHAR(100) NOT NULL,
-    email			VARCHAR(50) NOT NULL,
-    date_created	TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT unique_user UNIQUE(username),
-    CONSTRAINT unique_umail UNIQUE(email)
+  user_id       INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username      VARCHAR(20) NOT NULL,
+  `password`    VARCHAR(100) NOT NULL,
+  email         VARCHAR(50) NOT NULL,
+  date_created  TIMESTAMP NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT    unique_user UNIQUE(username),
+  CONSTRAINT    unique_umail UNIQUE(email)
 );
 */
--- migrations/20190321234006_create_memes_table.js
+++ migrations/20190321234006_create_memes_table.js
@@ -1,31 +1,29 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('memes', function(table) {
-    table.increments('meme_id');
-    table.integer('user_id').notNullable();
-    table.string('title', 50).notNullable();
-    table.string('cloudinary_url', 100).notNullable();
-    table.timestamp('date_created').defaultTo(knex.fn.now());
+exports.up = knex => knex.schema.createTable('memes', (table) => {
+  table.increments('meme_id');
+  table.integer('user_id').notNullable();
+  table.string('title', 50).notNullable();
+  table.string('cloudinary_url', 100).notNullable();
+  table.timestamp('date_created').defaultTo(knex.fn.now());
 
-    table.unique('cloudinary_url');
-    table.foreign('user_id').references('user_id').inTable('users').onDelete('cascade').onUpdate('cascade');
-  })
-};
+  table.unique('cloudinary_url');
+  table.foreign('user_id').references('user_id').inTable('users').onDelete('cascade')
+    .onUpdate('cascade');
+});
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('memes');
-};
+exports.down = knex => knex.schema.dropTable('memes');
 
 /*
 CREATE TABLE IF NOT EXISTS memes (
-	meme_id 		INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id			INT NOT NULL,
-    title			VARCHAR(50) NOT NULL,
-    cloudinary_url	VARCHAR(100) NOT NULL,
-    date_created 	TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT unique_cloudurl UNIQUE(cloudinary_url),
-    CONSTRAINT fk_user_memes FOREIGN KEY (user_id) REFERENCES users(user_id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+  meme_id         INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id         INT NOT NULL,
+  title           VARCHAR(50) NOT NULL,
+  cloudinary_url  VARCHAR(100) NOT NULL,
+  date_created    TIMESTAMP NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT unique_cloudurl UNIQUE(cloudinary_url),
+  CONSTRAINT fk_user_memes FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 */
