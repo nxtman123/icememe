24925415e2619bd6019ce19cadf50376bac34fb4
Kurtis Jantzen <kjjantze@ucalgary.ca>
2019-03-28 14:18:18 -0600

    Adjust meme card width design

--- src/components/MemeCard.vue
+++ src/components/MemeCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-12 col-sm-4">
+  <div class="col-12 col-sm-6 col-md-4">
     <q-card>
       <router-link
         :to="{ name: 'meme', params: { memeId, slug: slugTitle }}"
