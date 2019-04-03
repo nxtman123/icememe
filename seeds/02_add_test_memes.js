c1811685ac5510b52119d7efca29af12ed75bf23
adnanrh <arhusain@ucalgary.ca>
2019-04-02 23:25:22 -0600

    Adds image links to seeds and 2 comments per meme.

--- seeds/02_add_test_memes.js
+++ seeds/02_add_test_memes.js
@@ -8,38 +8,38 @@ exports.seed = function(knex, Promise) {
         {
           meme_id: 1, 
           user_id: 1,
-          title: 'Adams meme',
-          cloudinary_url: ''
+          title: 'Adam\'s meme',
+          cloudinary_url: 'https://i.imgflip.com/2xi8c8.jpg'
         },
         {
           meme_id: 2, 
           user_id: 2,
-          title: 'Adnans meme',
-          cloudinary_url: ''
+          title: 'Adnan\'s meme',
+          cloudinary_url: 'https://i.imgflip.com/2tx3fo.jpg'
         },
         {
           meme_id: 3, 
           user_id: 3,
-          title: 'Amairs meme',
-          cloudinary_url: ''
+          title: 'Amair\'s meme',
+          cloudinary_url: 'https://i.kym-cdn.com/entries/icons/original/000/021/311/free.jpg'
         },
         {
           meme_id: 4, 
           user_id: 4,
-          title: 'Farzams meme',
-          cloudinary_url: ''
+          title: 'Farzam\'s meme',
+          cloudinary_url: 'https://i.kym-cdn.com/photos/images/original/001/469/681/ed2.jpg'
         },
         {
           meme_id: 5, 
           user_id: 5,
-          title: 'Floras meme',
-          cloudinary_url: ''
+          title: 'Flora\'s meme',
+          cloudinary_url: 'https://i.kym-cdn.com/photos/images/newsfeed/001/004/013/679.jpg'
         },
         {
           meme_id: 6, 
           user_id: 6,
-          title: 'Kurtis meme',
-          cloudinary_url: ''
+          title: 'Kurtis\' meme',
+          cloudinary_url: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg'
         }
       ]);
     });
--- seeds/04_add_test_votes.js
+++ seeds/04_add_test_votes.js
@@ -14,31 +14,43 @@ exports.seed = function(knex, Promise) {
         {
           vote_id: 2, 
           user_id: 2,
-          meme_id: 2,
+          meme_id: 1,
           type: 'up'
         },
         {
           vote_id: 3, 
           user_id: 3,
-          meme_id: 3,
-          type: 'up'
+          meme_id: 1,
+          type: 'down'
         },
         {
           vote_id: 4, 
           user_id: 4,
-          meme_id: 4,
+          meme_id: 2,
           type: 'up'
         },
         {
           vote_id: 5, 
           user_id: 5,
-          meme_id: 5,
+          meme_id: 2,
           type: 'up'
         },
         {
           vote_id: 6, 
           user_id: 6,
-          meme_id: 6,
+          meme_id: 3,
+          type: 'up'
+        },
+        {
+          vote_id: 6, 
+          user_id: 2,
+          meme_id: 3,
+          type: 'down'
+        },
+        {
+          vote_id: 7, 
+          user_id: 1,
+          meme_id: 4,
           type: 'up'
         }
       ]);
