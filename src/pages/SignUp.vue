23c36628ebdcd25e1f804799c224c1033436b392
amairJD <amairj965@gmail.com>
2019-04-07 20:26:47 -0600

    User Page update. Wierd issue where text entry disspaears when clicking elsewhere.

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,53 +1,16 @@
 <template>
   <q-page padding>
     <h1>Registration</h1>
-    <q-field
-      label="Email"
-      :error="mailHasError"
-      error-label="We need a valid email"
-      >
-      <q-input v-model="text" stack-label="Stack Label" placeholder="Enter Email"/>
-    </q-field>
-    <q-field
-      label="username"
-      helper="Helper"
-      :error="userNameHasError"
-      error-label="We need a valid username"
-      :count="10"
-      >
-      <q-input v-model="text" stack-label="Stack Label" placeholder="Enter Username"/>
-    </q-field>
-
-    <q-field
-      label="Password"
-      helper="Helper"
-      :error="pwdHasError"
-      error-label="We need a valid password"
-      :count="10"
-      >
-      <q-input
-        v-model="text"
-        type="password"
-        stack-label="Stack Label"
-        placeholder="Enter Password"
-      />
-    </q-field>
-    <q-field
-      label="confirmPassword"
-      helper="Helper"
-      :error="cPwdHasError"
-      error-label="Password's do not match"
-      :count="10"
-      >
-      <q-input
-        v-model="text"
-        type="password"
-        stack-label="Stack Label"
-        placeholder="Confirm Password"
-      />
-    </q-field>
+    <form >
+        <q-input style="padding: 10px" v-model="inputMessage" outlined label="Email" />
+        <q-input style="padding: 10px" v-model="inputMessage" outlined label="UserName" />
+        <q-input style="padding: 10px" v-model="inputMessage" outlined label="Password"
+          type="password"/>
+          <q-input style="padding: 10px" v-model="inputMessage" outlined label="Confirm Password"
+            type="password"/>
+    </form>
     <div class="row justify-center full-height full-width text-center" style="margin: 30px">
-      <q-btn color="primary" label="Create Account" />
+      <q-btn color="primary" label="Create Account"  />
     </div>
   </q-page>
 </template>
