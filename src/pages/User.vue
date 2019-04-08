346932f38f62bbfdc69ec70f36bf26e3646ae915
amairJD <amairj965@gmail.com>
2019-04-07 22:11:55 -0600

    Created basic layout for Personal Page

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,12 +1,50 @@
 <template>
   <q-page padding>
-    <h1>User Profile Page</h1>
+    <p style="padding: 15px" >Posted by: john-cena</p>
+    <div class="row justify-center q-col-gutter-sm">
+      <meme-card v-bind="demoMeme" />
+      <meme-card v-bind="demoMeme" />
+      <meme-card v-bind="demoMeme" />
+      <meme-card v-bind="demoMeme" />
+      <meme-card v-bind="demoMeme" />
+    </div>
   </q-page>
 </template>
 
 <script>
+import MemeCard from '../components/MemeCard';
+
 export default {
   name: 'PageUser',
+  components: {
+    'meme-card': MemeCard,
+  },
+  data() {
+    return {
+      demoMeme: {
+        memeId: 0,
+        authorUsername: 'john-cena',
+        title: 'Mr. Fish, I don\'t feel so good',
+        cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+        dateCreated: 1554145159,
+        voteTotal: 543,
+        userVote: 'up',
+        commentCount: 17,
+      },
+      inputMessage: '',
+      serverMessage: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$socket.emit('hello', this.inputMessage);
+    },
+  },
+  sockets: {
+    hello(reply) {
+      this.serverMessage = reply;
+    },
+  },
 };
 </script>
 
