544ad802411456bc17d9b74bde1e8bd7babce559
Kurtis Jantzen <kjjantze@ucalgary.ca>
2019-04-08 20:27:06 -0600

    Start on main page design

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,53 +1,26 @@
 <template>
   <q-page class="column justify-center page-frame">
-    <p style="background-color: lightblue; padding: 16px;">
-      Links to pages for development
-      <br>
-      <router-link :to="{ name: 'register' }">
-        Sign Up
-      </router-link> |
-      <router-link :to="{ name: 'new' }">
-        New Meme
-      </router-link> |
-      <router-link :to="{ name: 'meme', params: { memeId: 123, slug: 'my-first-meme', }}">
-        Meme Page
-      </router-link> |
-      <router-link :to="{ name: 'user', params: { username: 'john-cena' }}">
-        User Profile
-      </router-link> |
-      <router-link :to="{ name: 'settings' }">
-        Settings
-      </router-link>
-    </p>
-    <div class="row justify-center q-col-gutter-sm">
-      <meme-card v-bind="demoMeme" />
-      <meme-card v-bind="demoMeme" />
-      <meme-card v-bind="demoMeme" />
-      <meme-card v-bind="demoMeme" />
-      <meme-card v-bind="demoMeme" />
-    </div>
-    <p>Send a message to the server. Choose an adjective.</p>
-    <template v-if="serverMessage">
-      <p
-        class="text-bold"
-        style="white-space: pre-line;"
-      >
-        {{ serverMessage }}
-      </p>
-      <q-btn @click="inputMessage = serverMessage = ''">
-        clear
-      </q-btn>
-    </template>
-    <form
-      v-else
-      @submit.prevent="onSubmit"
+    <q-infinite-scroll
+      :offset="250"
+      @load="loadMoreMemes"
     >
-      <q-input
-        v-model="inputMessage"
-        outlined
-        label="adjective"
-      />
-    </form>
+      <div class="row justify-center q-col-gutter-sm q-pt-md">
+        <meme-card
+          v-for="meme in memes"
+          :key="meme.id"
+          v-bind="meme"
+        />
+      </div>
+
+      <template v-slot:loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner
+            color="primary"
+            size="40px"
+          />
+        </div>
+      </template>
+    </q-infinite-scroll>
   </q-page>
 </template>
 
@@ -61,28 +34,120 @@ export default {
   },
   data() {
     return {
-      demoMeme: {
-        memeId: 0,
-        authorUsername: 'icedoge',
-        title: 'Mr. Fish, I don\'t feel so good',
-        cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
-        dateCreated: 1554145159,
-        voteTotal: 543,
-        userVote: 'up',
-        commentCount: 17,
-      },
-      inputMessage: '',
-      serverMessage: '',
+      memes: [
+        {
+          memeId: 0,
+          authorUsername: 'icedoge',
+          title: 'Mr. Fish, I don\'t feel so good',
+          cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+          dateCreated: 1554145159,
+          voteTotal: 543,
+          userVote: 'down',
+          commentCount: 17,
+        },
+        {
+          memeId: 1,
+          authorUsername: 'icedoge',
+          title: 'I can\'t do that while you\'re watching',
+          cloudinaryUrl: 'https://i.redd.it/0snws7y219b11.png',
+          dateCreated: 1554145159,
+          voteTotal: 54323,
+          userVote: 'up',
+          commentCount: 3123,
+        },
+        {
+          memeId: 2,
+          authorUsername: 'icedoge',
+          title: 'Mr. Fish, I don\'t feel so good',
+          cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+          dateCreated: 1554145159,
+          voteTotal: -543,
+          userVote: 'down',
+          commentCount: 17,
+        },
+        {
+          memeId: 3,
+          authorUsername: 'icedoge',
+          title: 'I can\'t do that while you\'re watching',
+          cloudinaryUrl: 'https://i.redd.it/0snws7y219b11.png',
+          dateCreated: 1554145159,
+          voteTotal: 54323,
+          userVote: 'up',
+          commentCount: 3123,
+        },
+        {
+          memeId: 4,
+          authorUsername: 'icedoge',
+          title: 'Mr. Fish, I don\'t feel so good',
+          cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+          dateCreated: 1554145159,
+          voteTotal: 543,
+          userVote: 'down',
+          commentCount: 17,
+        },
+        {
+          memeId: 5,
+          authorUsername: 'icedoge',
+          title: 'I can\'t do that while you\'re watching',
+          cloudinaryUrl: 'https://i.redd.it/0snws7y219b11.png',
+          dateCreated: 1554145159,
+          voteTotal: 54323,
+          userVote: 'up',
+          commentCount: 3123,
+        },
+        {
+          memeId: 6,
+          authorUsername: 'icedoge',
+          title: 'Mr. Fish, I don\'t feel so good',
+          cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+          dateCreated: 1554145159,
+          voteTotal: 543,
+          userVote: 'down',
+          commentCount: 17,
+        },
+        {
+          memeId: 7,
+          authorUsername: 'icedoge',
+          title: 'I can\'t do that while you\'re watching',
+          cloudinaryUrl: 'https://i.redd.it/0snws7y219b11.png',
+          dateCreated: 1554145159,
+          voteTotal: 54323,
+          userVote: 'up',
+          commentCount: 3123,
+        },
+      ],
     };
   },
   methods: {
-    onSubmit() {
-      this.$socket.emit('hello', this.inputMessage);
-    },
-  },
-  sockets: {
-    hello(reply) {
-      this.serverMessage = reply;
+    loadMoreMemes(index, done) {
+      setTimeout(() => {
+        if (this.memes) {
+          const nextId = this.memes.length;
+          this.memes.push(
+            {
+              memeId: nextId,
+              authorUsername: 'icedoge',
+              title: 'Mr. Fish, I don\'t feel so good',
+              cloudinaryUrl: 'https://i.kym-cdn.com/photos/images/original/001/367/501/600.jpg',
+              dateCreated: 1554145159,
+              voteTotal: 543,
+              userVote: 'down',
+              commentCount: 17,
+            },
+            {
+              memeId: nextId + 1,
+              authorUsername: 'icedoge',
+              title: 'I can\'t do that while you\'re watching',
+              cloudinaryUrl: 'https://i.redd.it/0snws7y219b11.png',
+              dateCreated: 1554145159,
+              voteTotal: 54323,
+              userVote: 'up',
+              commentCount: 3123,
+            },
+          );
+        }
+        done();
+      }, 2000);
     },
   },
 };
