f9d0279046aa0d0d34596dbf973d28c1c4d0f687
Farzam Noori <fnoori431@gmail.com>
2019-04-14 11:32:13 -0600

    Needed to add exception to rule, because it was throwing errors for underscore case, but the data coming from the db is underscorecase

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,8 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'prefer-promise-reject-errors': 'off',
-    
+    'vue/prop-name-casing': 'off',
+
     // allow console.log during development only
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
